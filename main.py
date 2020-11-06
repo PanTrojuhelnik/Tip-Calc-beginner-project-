d888f4ae8a198f52ba0e29c7b27d8f074edf3b13
Martin Dobiaš <dobiasma@gmail.com>
2020-11-06 21:52:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Welcome to the Tip calculator.")
+cena = float(input("What was the total bill? $"))
+dysko = int(input("what percentage tip would you like to give? 10, 12, or 15? "))
+lidi = int(input("How many people to split the bill? "))
+dysko_prevod = float((dysko / 100) + 1.00)
+vsecko = float((cena / lidi) * dysko_prevod)
+vse_dohromady = round(vsecko, 2)
+print(f"Each person should pay: {vse_dohromady}")
+
