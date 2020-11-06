ffd2cea280ce86f6a15967e87f8a3c3aaf5cb1bb
Martin Dobiaš <dobiasma@gmail.com>
2020-11-06 21:54:40 +0000

    just one letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Welcome to the Tip calculator.")
 cena = float(input("What was the total bill? $"))
-dysko = int(input("what percentage tip would you like to give? 10, 12, or 15? "))
+dysko = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 lidi = int(input("How many people to split the bill? "))
 dysko_prevod = float((dysko / 100) + 1.00)
 vsecko = float((cena / lidi) * dysko_prevod)
